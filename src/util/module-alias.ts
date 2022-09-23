248951677e94bade416d10f2bd40ff1476f39dda
unknown <mauricio_maletta@hotmail.com>
2022-09-23 15:49:29 -0300

    add comments to module-alias.ts

--- src/util/module-alias.ts
+++ src/util/module-alias.ts
@@ -1,9 +1,20 @@
-import * as path from 'path';
-import moduleAlias from 'module-alias';
+import * as path from "path";
+import moduleAlias from "module-alias";
 
-const files= path.resolve(__dirname, '../..');
+const files = path.resolve(__dirname, "../..");
 
 moduleAlias.addAliases({
-  '@src': path.join(files, 'src'),
-  '@test': path.join(files, 'test'),
-})
+  "@src": path.join(files, "src"),
+  "@test": path.join(files, "test"),
+});
+
+/**
+ * O compilador vai buildar esse arquivo em uma pasta separa chamada ./dist/util
+ * Acho que este e todos os arquivos gerados dentro de ./dist são importados assim que a aplicação inicia com yarn start
+ * então o moduleAlias já está rodando mapeando os arquivos com aliases declardos na função.
+ *
+ * A importação na ide e no typescript é feita com o mapeamento em tsconfig.
+ *
+ * Neste arquivo é feito o mapeamento para depois que o projeto é buildado, para que o mapeamento depois de compilado
+ * funcione assim como está descrito no tsconfig.
+ */
