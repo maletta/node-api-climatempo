aa8fcfad2a5ac2029be1554edfd1c27d80b7f625
unknown <mauricio_maletta@hotmail.com>
2022-10-01 20:12:10 -0300

    mock axios with jest

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -7,16 +7,21 @@ import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normali
 jest.mock('axios');
 
 describe('StormGlass client', () => {
+  // do mockedAxios have jest and axios types;
+  const mockedAxios = axios as jest.Mocked<typeof axios>;
   it('Should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.123123;
     const lng = 151.123123;
 
-    // mock axios data
-    axios.get = jest
-      .fn()
-      .mockReturnValue({ data: stormGlassWeather3HoursFixture });
+    // mock axios data avoid overriding the axios function
+    mockedAxios.get.mockResolvedValue({ data: stormGlassWeather3HoursFixture });
+    // dont need overriding because we have mocked axios with jest.mock("axios")
+    // mock axios data overriding axios function
+    // axios.get = jest
+    //   .fn()
+    //   .mockReturnValue({ data: stormGlassWeather3HoursFixture });
 
-    const stormGlass = new StormGlass(axios);
+    const stormGlass = new StormGlass(mockedAxios);
     const response = await stormGlass.fetchPoints(lat, lng);
     expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
