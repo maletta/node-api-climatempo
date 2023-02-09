2268a2a5f890dd409e727488004234ea791acf0a
mauricio maletta <mauricio_maletta@hotmail.com>
2023-02-09 18:04:34 -0300

    add comments to declaration types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,7 @@
 import * as http from 'http';
 import { DecodedUser } from '@src/services/auth';
 
+// module augmentation
 declare module 'express-serve-static-core' {
   export interface Request extends http.IncomingMessage, Express.Request {
     decoded?: DecodedUser
