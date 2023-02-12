4fba33dddd265930db78b574be76ed5adc4a0f1a
Mauricio Maletta <mauricio_maletta@hotmail.com>
2023-02-12 20:01:33 -0300

    add database options to mongoose connect

--- src/database.ts
+++ src/database.ts
@@ -6,7 +6,17 @@ const dbConfig: IConfig = config.get('App.database');
 export const connect = async (): Promise<void> => {
   console.log('------------------ database log -------------------------');
   console.log('mongo url', dbConfig.get('mongoUrl'));
-  await mongooseConnect(dbConfig.get('mongoUrl'));
+  await mongooseConnect(dbConfig.get('mongoUrl'), {
+    // auth: { authSource: 'admin' },
+    user: process.env.MONGOUSER,
+    pass: process.env.MONGOPASSWORD,
+    dbName: 'surf-forecast',
+  }).then((error) => {
+    if (error) {
+      console.log(error);
+      return console.log('Unable to connect to the db.');
+    }
+  });
 };
 
 export const close = (): Promise<void> => connection.close();
