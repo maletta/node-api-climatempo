d72384789de8933935dc03bec55bcdd50a4cadd6
Mauricio Maletta <mauricio_maletta@hotmail.com>
2023-02-12 18:38:39 -0300

    change mongo_url_database; add logs

--- src/database.ts
+++ src/database.ts
@@ -3,9 +3,10 @@ import { connect as mongooseConnect, connection } from 'mongoose';
 
 const dbConfig: IConfig = config.get('App.database');
 
-
 export const connect = async (): Promise<void> => {
-  await  mongooseConnect(dbConfig.get('mongoUrl'));
+  console.log('------------------ database log -------------------------');
+  console.log('mongo url', dbConfig.get('mongoUrl'));
+  await mongooseConnect(dbConfig.get('mongoUrl'));
 };
 
 export const close = (): Promise<void> => connection.close();
