416a92c71f2e11578375a1b75e1048aa66a9f095
Mauricio Maletta <mauricio_maletta@hotmail.com>
2023-04-26 19:05:20 -0300

    add rate limit to avoid ddos attacks

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,16 +1,35 @@
-import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get, Middleware } from '@overnightjs/core';
 import logger from '@src/logger';
 import { authMiddleware } from '@src/middleware/auth';
 import { Beach } from '@src/models/beach';
 import { Forecast } from '@src/services/forecast';
 import { Request, Response } from 'express';
 import { BaseController } from '.';
+import { rateLimit } from 'express-rate-limit';
+import ApiError from '@src/util/errors/api-errors';
+
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000,
+  max: 1,
+  keyGenerator(req: Request) : string {
+    return req.ip;
+  },
+  handler(_, res: Response) : void {
+    res.status(429).send(
+      ApiError.format({
+        code: 429,
+        message:'To many requests to the /forecast endpoint',
+      })
+    )
+  },
+})
 
 const forecast = new Forecast();
 @Controller('forecast')
 @ClassMiddleware(authMiddleware)
 export class ForecastController extends BaseController {
   @Get('')
+  @Middleware(rateLimiter)
   public async getForecastForLoggedUser(
     req: Request,
     res: Response
